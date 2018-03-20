150364083b4e473213b2495f2701010419e421b3
VickieYan <me@wenjieyan.com>
2018-03-20 15:43:29 +0800

    :beers: code buffer

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# ParcelKit
-This repository has basic settings for buildling react application in Typescript.
-## Built in settings
-* Sass(if you don't use styled-components) 
-* Babel
-* PostCSS(autoprefixer)
-* PostHTML
-* TypeScript(without any additional configuration)
-* React
-
+# Reborn
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { injectGlobal } from 'styled-components'
+import reset from 'styled-reset'
+import Nav from './components/Nav'
 
-const App = () => (<div>Look at the star, look how they shine for you.</div>)
+const baseStyles = () => injectGlobal`
+    ${reset}
+`
+
+const App = () => {
+    baseStyles()
+    return <Nav />
+}
 
 export default App
